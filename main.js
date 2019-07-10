576e4269eeebdb96c7e8734b07958b5c4c3b3a94
Michał Janik <mjanik45@gmail.com>
2019-07-10 18:22:47 +0200

    Change part 6

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
 const btnHref = document.querySelector('header button');
 btnHref.addEventListener('click', () => location.href = "https://github.com/michaljanik99?tab=repositories");
+
+
+$(window).on("scroll", function () {
+    let windowHeigth = $(window).height();
+    let windowWidth = $(window).width();
+    let scrollValue = $(this).scrollTop();
+
+    const $aboutMe = $('.about_me');
+    const aboutMeFromTop = $aboutMe.offset().top;
+    const aboutMeHeight = $aboutMe.outerHeight()
+
+    if (scrollValue > aboutMeFromTop + ((aboutMeHeight / 2) - windowHeigth)) {
+        $('.about_me .sentence').addClass('active')
+
+
+
+    }
+
+
+});
