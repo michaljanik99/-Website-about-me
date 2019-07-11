5f93bd9fbaac43f7c9ace5150a7841f0bf9a8b3f
Michał Janik <mjanik45@gmail.com>
2019-07-11 14:03:39 +0200

    Change site part 6

--- main.js
+++ main.js
@@ -1,33 +1,30 @@
 const btnHref = document.querySelector('header button');
 btnHref.addEventListener('click', () => location.href = "https://github.com/michaljanik99?tab=repositories");
-const textArea = document.querySelector('.about_me h1');
-const txt = 'TWORZE NOWOCZESNE STRONY INTERNETOWE';
-
-
-
-
-
-
-
-let windowHeigth = $(window).height();
-let windowWidth = $(window).width();
-let a = $('.about_me').offset().top;
-let b = $('.about_me').outerHeight();
-
-
 
 
 $(window).on("scroll", function () {
     let windowHeigth = $(window).height();
     let windowWidth = $(window).width();
     let scrollValue = $(this).scrollTop();
-    let f = true
-    indexText = 0;
+
     const $aboutMe = $('.about_me');
     const aboutMeFromTop = $aboutMe.offset().top;
     const aboutMeHeight = $aboutMe.outerHeight()
 
-    if (scrollValue >= aboutMeFromTop + ((aboutMeHeight / 2) - windowHeigth)) {
+    const $html = $('.project.html');
+    const htmlFromTop = $html.offset().top;
+    const htmlHeight = $html.outerHeight()
+
+    const $css = $('.project.css');
+    const cssFromTop = $css.offset().top;
+    const cssHeight = $css.outerHeight()
+
+    const $js = $('.project.js');
+    const jsFromTop = $js.offset().top;
+    const jsHeight = $js.outerHeight()
+
+
+    if (scrollValue >= aboutMeFromTop + ((aboutMeHeight / 2) - windowHeigth) && windowWidth >= 1600) {
         $('.about_me .sentence').each(function (index, element) {
 
             $(element).css({
@@ -35,12 +32,19 @@ $(window).on("scroll", function () {
                 transition: `1s ${(index+1)/2}s linear`
             })
         })
-
-
-
-
-
     }
+    if (scrollValue >= htmlFromTop + ((htmlHeight / 2) - windowHeigth) && windowWidth >= 1600) {
+        $html.css({
+            left: 0
+        })
+    }
+    if (scrollValue >= cssFromTop + ((cssHeight / 2) - windowHeigth) && windowWidth >= 1600) {
+        $css.css({
+            left: 0
+        })
+    }
+
+
 
 
 
