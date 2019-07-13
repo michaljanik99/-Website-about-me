fd7535877304d09ffb781a1ceb892e18faf94240
Michał Janik <mjanik45@gmail.com>
2019-07-13 20:15:09 +0200

    End par 3

--- mail.php
+++ mail.php
@@ -1,16 +1,36 @@
 <?php
 if(isset($_POST['submit'])){
     
-    $to = 'concat@michaljanik.epizy.com';
+    // $to = 'concat@michaljanik.epizy.com';
     $name =$_POST['name'];
     $email = $_POST['email'];
-    $subject = 'Nowy mail od'.$name. 'z adresu ('.$email.')';
+    // $subject = 'Nowy mail od'.$name. 'z adresu ('.$email.')';
     $message = $_POST['message'];
-    $headers = 'Nowy mail od'.$name. 'z adresu ('.$email.')';
-    $headers = 'Content-type: text/html; charset=iso-8859-1';    
+    // $headers = 'Nowy mail od'.$name. 'z adresu ('.$email.')';
+    // $headers = 'Content-type: text/html; charset=iso-8859-1';    
     
     
-    mail($to, $subject, $message, $headers);
-    header('Location:index.html');
+    // mail($to, $subject, $message, $headers);
+    // header('Location:index.php');
+    require 'PHPMailerAutoload.php';
+    $mail = new PHPMailer;
+    
+    $mail->Host ='smtp.gmail.com';
+    $mail->Port= '587';
+    $mail->SMTPAuth=true;
+    $mail->SMTPSecure='tls';
+    $mail->Username='michaljanikwebdesign@gmail.com';
+    $mail->Password='****';
+
+    $mail->setFrom($email,$name);
+    $mail->addAddress('mjanik45@gmail.com');
+    $mail->addReplyTo($email,$name);
+    $mail->isHTML(true);
+    $mail->Subject="FF";
+    $mail->Body = $message;
+
+    if($mail->send()){
+        header('Location:index.html');
+    }
     
 };
